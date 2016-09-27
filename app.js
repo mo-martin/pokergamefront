9c95faa1b98aeba049ef76d29c63bb376a99c4e6
Alan Bonham <alan.bonham@skybettingandgaming.com>
2016-09-27 14:42:29 +0100

    hover works....sorta

---
+++ app.js
@@ -0,0 +1,18 @@
+$(function() {
+    var $document = $(document);
+    var selector = '[data-rangeslider]';
+    var $element = $(selector);
+    var textContent = ('textContent' in document) ? 'textContent' : 'innerText';
+
+    function valueOutput(element) {
+        var value = element.value;
+        var output = element.parentNode.getElementsByTagName('output')[0] || element.parentNode.parentNode.getElementsByTagName('output')[0];
+        output[textContent] = value;
+        //console.log(value);
+    }
+
+    $document.on('input', 'input[type="range"], ' + selector, function(e) {
+            valueOutput(e.target);
+        });
+});
+
