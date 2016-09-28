3a87301acbc7e7c2ada7dd8803243e9150d2eb71
Alan Bonham <alan.bonham@skybettingandgaming.com>
2016-09-28 10:45:44 +0100

    fix jquery issue

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function () {
     var $document = $(document);
     var selector = '[data-rangeslider]';
     var $element = $(selector);
@@ -8,31 +8,19 @@ $(function() {
         var value = element.value;
         var output = element.parentNode.getElementsByTagName('output')[0] || element.parentNode.parentNode.getElementsByTagName('output')[0];
         output[textContent] = value;
-        //console.log(value);
+        console.log(value);
     }
 
     $document.on('input', 'input[type="range"], ' + selector, function(e) {
             valueOutput(e.target);
         });
 
-//////////hover////////////////////////
 
-$('.card').hover(function() {
-    $(this).css("cursor", "pointer");
-    $(this).css('visibility', 'hidden').hide().delay(1000).fadeOut();
-    $(this).animate({
-        width: "150%",
-        height: "150%"
-    }, 'slow');
 
-}, function() {
-    $(this).animate({
-        width: "100%",
-        height: "100%"
-    }, 'slow');
+// //chips validation on raise
+// function playerBet(){
+
+// };
 
 });
 
-
-
-});
