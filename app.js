ad167874dad37e276fbb63a8ebb7f0ad31cc0e29
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-07 09:49:16 +0100

    app.js code refined for docker

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.static('public'));
 app.use(express.static('bower_components'));
 
 app.use('/',function(req, res) {
-	res.render('index' , {api: process.env.API_URL || "http://192.10.10.150"});
+	res.render('index' , {api: process.env.API_URL || "http://localhost:3001/"});
 });
 
 app.listen(process.env.PORT || 3000, function() {
