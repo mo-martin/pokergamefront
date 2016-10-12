e07263b7bce885ed1410014974ec6c4b5e0a5e3b
Moses Martin <moses.martin@skybettingandgaming.com>
2016-10-12 09:43:00 +0100

    initial setup - private ip

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -12,6 +13,32 @@ app.use(express.static('bower_components'));
 app.use('/',function(req, res) {
 	res.render('index' , {api: process.env.API_URL || "http://localhost:3001/"});
 });
+
+
+app.use('/decknew',function(reqFromClient, resFromClient) {
+	var options = {
+	  host: 'api',
+	  path: '/Deck/new',
+	  port: '3000',
+	};
+
+	callback = function(responseFromApi) {
+	  var str = ''
+	  responseFromApi.on('data', function (chunk) {
+	    str += chunk;
+	  });
+
+	  responseFromApi.on('end', function () {
+	    console.log(str);
+			resFromClient.setHeader('Content-Type', 'application/json')
+			resFromClient.send(str);
+	  });
+	}
+
+	var reqToApi = http.request(options, callback);
+	reqToApi.end();
+});
+
 
 app.listen(process.env.PORT || 3000, function() {
 	console.log('server has started');
